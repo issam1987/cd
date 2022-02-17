8a02ffd763d2a4fdd557ab72ce43d34d6ab2870b
Ghorbali Issam <issam.ghorbali@external.stellantis.com>
2022-02-17 17:44:07 +0100

    first test310510123191018

--- src/index.php
+++ src/index.php
@@ -2,7 +2,10 @@
 /**
  * Define template file.
  * 
+ * This function calls a static fetching method against the Ingredient class
+ * 
  * @param int $a Describe what this parameter is
+ * @return int
  */
 function hookMenu($a){
     return $a+3;

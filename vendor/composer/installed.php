430f3e0b9714ed25ae51384dba602e3eb440cdb6
Ghorbali Issam <issam.ghorbali@external.stellantis.com>
2022-02-17 16:04:44 +0100

    first test

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '27792b45951e8d813d748e72da297447939a0110',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '27792b45951e8d813d748e72da297447939a0110',
+            'dev_requirement' => false,
+        ),
+    ),
+);
